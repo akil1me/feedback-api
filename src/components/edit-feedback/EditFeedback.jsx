2f442af1a817f364501d19fe752b35d7414d25cf
akil1me <akipax123@gmail.com>
2022-11-16 23:36:06 +0500

    fix netlify

--- src/components/edit-feedback/EditFeedback.jsx
+++ src/components/edit-feedback/EditFeedback.jsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../../App";
 import { FeedBtn } from "../button/";
 import GoBack from "../go-back/GoBack";
@@ -12,7 +11,6 @@ export const EditeFeedback = () => {
     <div className="d-flex justify-content-between align-items-center mt-4">
       <GoBack />
       <FeedBtn link={login ? "edite-feedback" : "/login"} edit="editBtn" text="Edit Feedback" classs={"addBtn"} ></FeedBtn>
-
     </div>
   )
 }
